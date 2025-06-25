7717a627431932d8398b9acf63d3feaf29409142
Emily Sadler <emily@sadler.gg>
2025-06-25 15:54:21 +0100

    change to welcome message so it doesn't overlap on smaller screens

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const app = require("./app.js");
-// const { connectToDatabase } = require("./db/db.js");
+// const app = require("./app.js");
+// // const { connectToDatabase } = require("./db/db.js");
 
-function listenForRequests() {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log("Now listening on port", port);
-  });
-}
+// function listenForRequests() {
+//   const port = process.env.PORT || 3001;
+//   app.listen(port, () => {
+//     console.log("Now listening on port", port);
+//   });
+// }
 
-listenForRequests();
+// listenForRequests();
 
 // connectToDatabase().then(() => {
 //   listenForRequests();
