6c42d8bdc4ff2a6ab45880000d1a6cc4cc97adea
Emily Sadler <emily@sadler.gg>
2025-06-25 17:22:15 +0100

    fixed tailwind dependencies and versions

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     // Prevent Rollup from trying to bundle dependencies that cause issues
     commonjsOptions: {
