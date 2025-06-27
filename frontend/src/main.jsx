e4798d1be8264f60c28df6ce8d6949d74eb1ea50
Emily Sadler <emily@sadler.gg>
2025-06-27 14:06:30 +0100

    Added sentry

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,15 @@ import React from "react";
 import App from "./App.jsx";
 import "./index.css";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://56ad4d8b419908adbb636c6e82ca0ef3@o4509570787966976.ingest.de.sentry.io/4509570789212240",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
+
 const rootElement = document.getElementById("root");
 
 // createRoot(document.getElementById("root")).render(
