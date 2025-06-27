c610552147a508bf966d2691f0039998e972d2c7
Emily Sadler <emily@sadler.gg>
2025-06-27 11:34:37 +0100

    Changes to versions to support backend

--- backend/routes/tmdb.js
+++ backend/routes/tmdb.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get("/initialMovie", TMDBController.getRandomMovie);
 router.get("/cast/:id", TMDBController.getCastFromMovieId);
-router.get("/search/:name", TMDBController.getSearchResults)
+router.get("/search/:name", TMDBController.getSearchResults);
 
 module.exports = router;
